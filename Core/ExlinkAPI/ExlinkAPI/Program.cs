755526ac17913aab6d0621b438aace1a0db4df18
Venkat <venkatarao.p@gmail.com>
2026-02-14 12:14:55 +1100

    ExlinkAPI project is in working condition. When run, the swagger home page is loading by default.

--- Core/ExlinkAPI/ExlinkAPI/Program.cs
+++ Core/ExlinkAPI/ExlinkAPI/Program.cs
@@ -3,23 +3,42 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. Define a name for the policy
+var myAngularPolicy = "AllowAngularApp";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddDbContext<ExdocContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// 2. Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAngularPolicy,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200") // Angular's default port
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty; // This sets Swagger as the root page
+    });
 }
 
 app.UseHttpsRedirection();
